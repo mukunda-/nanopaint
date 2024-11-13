9a2a43391ae247bf3bb37d206e31b59f6633808b
Mukunda Johnson <mukunda@mukunda.com>
2024-11-13 12:30:39 -0600

    WIP

--- server/core/block-repo_test.go
+++ server/core/block-repo_test.go
@@ -5,11 +5,16 @@
 package core
 
 func TestBlockRepo(t *testing.T) {
+	var repo BlockRepo
 	app := fxtest.New(
 		fx.Provide(CreateTestBlockRepo),
+		fx.Invoke(func(prepo BlockRepo) {
+			repo = prepo
+		}),
 	).RequireStart()
 
 	defer app.RequireStop()
 
-
+	// 
+	repo.SetPixel(BlockCoords{0, 0}, Color{0, 0, 0})
 }
